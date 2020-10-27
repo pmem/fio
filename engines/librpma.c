711115cfe238fb1d2a6f062cb5d700d5ac538a9b
osalyk <oksana.salyk@intel.com>
2020-10-27 07:58:38 +0100

    rpma: add write mode

--- engines/librpma.c
+++ engines/librpma.c
@@ -523,6 +523,7 @@ static int client_commit(struct thread_data *td)
 	bool fill_time;
 	int ret;
 	int i;
+	unsigned long long flush_len = 0;
 
 	if (!cd->io_us_queued)
 		return -1;
@@ -531,13 +532,28 @@ static int client_commit(struct thread_data *td)
 	for (i = 0; i < cd->io_u_queued_nr; i++) {
 		struct io_u *io_u = cd->io_us_queued[i];
 
-		if (i == cd->io_u_queued_nr - 1)
-			flags = RPMA_F_COMPLETION_ALWAYS;
-
 		if (io_u->ddir == DDIR_READ) {
+			if (i == cd->io_u_queued_nr - 1)
+				flags = RPMA_F_COMPLETION_ALWAYS;
 			/* post an RDMA read operation */
 			if ((ret = client_io_read(td, io_u, flags)))
 				return -1;
+		} else if (io_u->ddir == DDIR_WRITE) {
+			/* XXX - random read/write */
+			/* post an RDMA write operation */
+			if ((ret = client_io_write(td, io_u, RPMA_F_COMPLETION_ON_ERROR)))
+				return -1;
+			flush_len += io_u->xfer_buflen;
+			if (i == cd->io_u_queued_nr - 1) {
+				ret = rpma_flush(cd->conn, cd->server_mr,
+					io_u->offset, flush_len,
+					cd->flush_type, RPMA_F_COMPLETION_ALWAYS,
+					(void *)(uintptr_t)io_u->index);
+				if (ret) {
+					rpma_td_verror(td, ret, "rpma_flush");
+					return -1;
+				}
+			}
 		} else {
 			log_err("unsupported IO mode: %s\n", io_ddir_name(io_u->ddir));
 			return -1;
