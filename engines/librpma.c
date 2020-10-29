dd3588da8185115b4a12101714a0089d279205ae
osalyk <oksana.salyk@intel.com>
2020-10-29 08:37:20 +0100

    rpma: mix read/write

--- engines/librpma.c
+++ engines/librpma.c
@@ -123,12 +123,6 @@ static int client_init(struct thread_data *td)
 	rpma_log_set_threshold(RPMA_LOG_THRESHOLD, RPMA_LOG_LEVEL_INFO);
 	rpma_log_set_threshold(RPMA_LOG_THRESHOLD_AUX, RPMA_LOG_LEVEL_ERROR);
 
-	/* XXX not supported readwrite= rw/randwrite/randrw */
-	if (td_random(td) || td_rw(td)) {
-		log_err("Not supported mode. ");
-		return 1;
-	}
-
 	/* allocate client's data */
 	cd = calloc(1, sizeof(struct client_data));
 	if (cd == NULL) {
@@ -556,45 +550,61 @@ static int client_commit(struct thread_data *td)
 	bool fill_time;
 	int ret;
 	int i;
-	unsigned long long flush_len = 0;
+	int last_read;
+	unsigned long long int flush_len = 0;
 
 	if (!cd->io_us_queued)
 		return -1;
+
+	/* find last read operation */
+	last_read = cd->io_u_queued_nr - 1;
+	if (cd->io_us_queued[last_read]->ddir != DDIR_READ) {
+		do {
+			last_read --;
+		} while (!last_read || cd->io_us_queued[last_read]->ddir == DDIR_READ);
+	}
 
 	/* execute all io_us from queued[] */
 	for (i = 0; i < cd->io_u_queued_nr; i++) {
 		struct io_u *io_u = cd->io_us_queued[i];
 
 		if (io_u->ddir == DDIR_READ) {
-			if (i == cd->io_u_queued_nr - 1)
+			if (last_read == cd->io_u_queued_nr - 1)
 				flags = RPMA_F_COMPLETION_ALWAYS;
 			/* post an RDMA read operation */
 			if ((ret = client_io_read(td, io_u, flags)))
 				return -1;
 		} else if (io_u->ddir == DDIR_WRITE) {
-/*
- * XXX this implementation works only for readwrite=read,write
- * - to support readwrite=rw it has to consider interlaced sequences
- *   of reads and writes
- * - to support readwrite=randwrite,randrw it has to consider
- *   flush_len for nonadjacent writes
- */
 			/* post an RDMA write operation */
-			if ((ret = client_io_write(td, io_u, RPMA_F_COMPLETION_ON_ERROR)))
-				return -1;
+			if (td_random(td) || (td_random(td) && td_write(td))) {
+				ret = client_io_write(td, io_u, RPMA_F_COMPLETION_ON_ERROR);
+				if (ret)
+					return -1;
+				ret = rpma_flush(cd->conn, cd->server_mr,
+					io_u->offset, io_u->xfer_buflen,
+					cd->flush_type, RPMA_F_COMPLETION_ALWAYS,
+					(void *)(uintptr_t)io_u->index);
+				if (ret) {
+					rpma_td_verror(td, ret, "rpma_flush");
+					return -1;
+				}
+			} else {
+				ret = client_io_write(td, io_u, RPMA_F_COMPLETION_ON_ERROR);
+				if (ret)
+					return -1;
 
-			flush_len += io_u->xfer_buflen;
+				flush_len += io_u->xfer_buflen;
 
-			if (i < cd->io_u_queued_nr - 1)
-				continue;
+				if ((i < cd->io_u_queued_nr - 1) || (cd->io_us_queued[i + 1]->ddir == DDIR_WRITE))
+					continue;
 
-			ret = rpma_flush(cd->conn, cd->server_mr,
-				io_u->offset, flush_len,
-				cd->flush_type, RPMA_F_COMPLETION_ALWAYS,
-				(void *)(uintptr_t)io_u->index);
-			if (ret) {
-				rpma_td_verror(td, ret, "rpma_flush");
-				return -1;
+				ret = rpma_flush(cd->conn, cd->server_mr, io_u->offset,
+					flush_len, cd->flush_type, RPMA_F_COMPLETION_ALWAYS,
+					(void *)(uintptr_t)io_u->index);
+				if (ret) {
+					rpma_td_verror(td, ret, "rpma_flush");
+					return -1;
+				}
 			}
 		} else {
 			log_err("unsupported IO mode: %s\n", io_ddir_name(io_u->ddir));
