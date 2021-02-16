bef23219f3ec2a47ef0a1236179981037af83fa3
Lukasz Dorau <lukasz.dorau@intel.com>
2021-02-16 11:38:38 +0100

    rpma: download archive of librpma using curl

--- ci/actions-install-librpma.sh
+++ ci/actions-install-librpma.sh
@@ -2,14 +2,15 @@
 
 # 11.02.2021 Merge pull request #866 from ldorau/rpma-mmap-memory-for-rpma_mr_reg-in-rpma_flush_apm_new
 LIBRPMA_VERSION=fbac593917e98f3f26abf14f4fad5a832b330f5c
+ZIP_FILE=rpma.zip
 
 WORKDIR=$(pwd)
 
 # install librpma
-git clone https://github.com/pmem/rpma.git
-mkdir -p rpma/build
-cd rpma/build
-git checkout -b v$LIBRPMA_VERSION $LIBRPMA_VERSION
+wget -O $ZIP_FILE https://github.com/pmem/rpma/archive/${LIBRPMA_VERSION}.zip
+unzip $ZIP_FILE
+mkdir -p rpma-${LIBRPMA_VERSION}/build
+cd rpma-${LIBRPMA_VERSION}/build
 cmake .. -DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX=/usr \
 	-DBUILD_DOC=OFF \
@@ -18,4 +19,4 @@ cmake .. -DCMAKE_BUILD_TYPE=Release \
 make -j$(nproc)
 sudo make -j$(nproc) install
 cd $WORKDIR
-rm -rf rpma
+rm -rf $ZIP_FILE rpma-${LIBRPMA_VERSION}
